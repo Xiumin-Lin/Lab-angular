01688cc9796206ba7871cfcf360016fb079187b1
Xiumin-Lin <60708580+Xiumin-Lin@users.noreply.github.com>
2022-06-14 02:07:30 +0200

    try component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { POKEMONS } from './mock-pokemon-list';
+import { Pokemon } from './pokemon';
 
 @Component({
   selector: 'app-root',
-  template: `<h1>Welcome to {{title}}!</h1>`
+  template: `<h1>Liste de Pokémon</h1>`
 })
-export class AppComponent {
-  title = 'Lab-angular';
+export class AppComponent implements OnInit {
+  pokemonList: Pokemon[] = POKEMONS;
+
+  // constructor() { } ❌ utiliser ngOnInit pour l'initialisation
+
+  ngOnInit(): void {
+    console.table(this.pokemonList)
+    this.selectPokemon(this.pokemonList[0])
+  }
+
+  selectPokemon(pokemon: Pokemon) {
+    console.log(`Vous avez cliqué sur le pokémon ${pokemon.name}`)
+  }
 }
