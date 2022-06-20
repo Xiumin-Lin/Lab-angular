9db9cc356debb780cbb92cdda8605e3c2c5083fa
Xiumin-Lin <60708580+Xiumin-Lin@users.noreply.github.com>
2022-06-20 20:51:51 +0200

    move file in pokemon module
    + update route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DetailPokemonComponent } from './detail-pokemon/detail-pokemon.component';
-import { ListPokemonComponent } from './list-pokemon/list-pokemon.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { PageNotFoundComponent } from './pokemon/page-not-found/page-not-found.component';
 
 // Angular lit les routes du haut vers le bas donc il faut faire attention à l'ordre des paths
 const routes: Routes = [
-  { path: 'pokemons', component: ListPokemonComponent },
-  { path: 'pokemon/:id', component: DetailPokemonComponent },
   { path: '', redirectTo: 'pokemons', pathMatch: 'full' },    // '' représente une route vide
   { path: '**', component: PageNotFoundComponent }  // ** permet d'intercepter tous les routes
 ];
